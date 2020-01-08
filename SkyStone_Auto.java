6fbddcf01315c5d4802a1af2c23c4bb88725eabb
Owen_whitman <owen@whitmans.us>
2020-01-08 10:03:33 -0500

    Update SkyStone_Auto.java

--- SkyStone_Auto.java
+++ SkyStone_Auto.java
@@ -5,9 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
@@ -31,7 +33,7 @@ import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocaliz
 public class SkyStone_Auto extends LinearOpMode {
     // motors
     private DcMotor fl, fr, bl, br, ta, da, md;
-
+    private Servo sv;
     //the gyroscope
     private BNO055IMU imu;
 
@@ -39,7 +41,7 @@ public class SkyStone_Auto extends LinearOpMode {
     private Orientation lastAngles = new Orientation();
     private double globalAngle, correction, cAngle = 0;
 
-    private static final double STRAFE = 24.5, MOVE = 19.16, TURN = 14.55; //taken from last years have to set
+    private static final double STRAFE = 27, MOVE = 19.16, TURN = 14.55; //taken from last years have to set
 
 
     /* Variables for the detection */
@@ -71,6 +73,7 @@ public class SkyStone_Auto extends LinearOpMode {
     // Class Members
     private OpenGLMatrix lastLocation = null;
     private VuforiaLocalizer vuforia = null;
+    boolean strafed = false;
     private boolean targetVisible = false;
     private float phoneXRotate    = 0;
     private float phoneYRotate    = 0;
@@ -85,7 +88,7 @@ public class SkyStone_Auto extends LinearOpMode {
 
         if (fr.getCurrentPosition() > fr.getTargetPosition())
             while ( opModeIsActive() && fr.getCurrentPosition() > fr.getTargetPosition() ) {
-                correction = checkDirection(cAngle, 0.03);
+                //correction = checkDirection(cAngle, 0.03);
                 telemetry.addData("forward, encoder",fr.getCurrentPosition());
                 fl.setPower(speed - correction);
                 fr.setPower(speed);
@@ -96,7 +99,7 @@ public class SkyStone_Auto extends LinearOpMode {
             }
         else
             while ( opModeIsActive() && fr.getCurrentPosition() < fr.getTargetPosition() ) {
-                correction = checkDirection(cAngle, 0.03);
+                //correction = checkDirection(cAngle, 0.03);
                 telemetry.addData("backward, encoder",fr.getCurrentPosition());
                 fl.setPower(-speed + correction);
                 fr.setPower(-speed);
@@ -120,11 +123,11 @@ public class SkyStone_Auto extends LinearOpMode {
     // modified once
     private void turnTo(double degrees, double speed) {
         cAngle += degrees;
-        turnTo(degrees, speed, true);
+        turnToo(degrees, speed, true);
     }
 
     //DONT USE THIS ONE
-    private void turnTo(double degrees, double speed, boolean lol) {
+    private void turnToo(double degrees, double speed, boolean lol) {
         fr.setTargetPosition( (int) -Math.round(degrees * TURN) + fr.getCurrentPosition() );
         double target = getAngle() - degrees;
 
@@ -164,25 +167,28 @@ public class SkyStone_Auto extends LinearOpMode {
 
     // strafes a certain number of centimeters (not recommended)
     private void strafeTo(double cen, double pow) {
-        fr.setTargetPosition( (int) -Math.round(cen * STRAFE) + fr.getCurrentPosition() );
-
+        fr.setTargetPosition( (int) -Math.round(cen * STRAFE) + fr.getCurrentPosition());
+        telemetry.addData("Status", fr.getTargetPosition());
+        telemetry.addData("Status", fr.getCurrentPosition());
+        telemetry.update();
         if (fr.getCurrentPosition() < fr.getTargetPosition())
             while ( opModeIsActive() && fr.getCurrentPosition() < fr.getTargetPosition() ) {
-                correction = checkDirection(cAngle, 0.02);
+                //correction = checkDirection(cAngle, 0.001);
 
-                fl.setPower(-pow + correction);
-                fr.setPower(pow - correction);
-                bl.setPower(pow);
-                br.setPower(-pow);
+                fl.setPower(-0.5 );
+                fr.setPower(0.5);
+                bl.setPower(0.5);
+                br.setPower(-0.5);
             }
         else
             while ( opModeIsActive() && fr.getCurrentPosition() > fr.getTargetPosition() ) {
-                correction = checkDirection(cAngle, 0.02);
+                //correction = checkDirection(cAngle, 0.001);
 
-                fl.setPower(pow - correction);
-                fr.setPower(-pow + correction);
-                bl.setPower(-pow);
-                br.setPower(pow);
+
+                fl.setPower(0.5);
+                fr.setPower(-0.5);
+                bl.setPower(-0.5);
+                br.setPower(0.5);
             }
 
         fl.setPower(0);
@@ -259,16 +265,16 @@ public class SkyStone_Auto extends LinearOpMode {
             idle();
     }
 
+
     /**
      * Code for the detection code
-     *
      */
-    private VuforiaTrackables detection(){
+    private VuforiaTrackables detection() {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
         // VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraDirection   = CAMERA_CHOICE;
+        parameters.cameraDirection = CAMERA_CHOICE;
         //  Instantiate the Vuforia engine
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
         // Load the data sets for the trackable objects. These particular data
@@ -342,7 +348,7 @@ public class SkyStone_Auto extends LinearOpMode {
 
         front1.setLocation(OpenGLMatrix
                 .translation(-halfField, -quadField, mmTargetHeight)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0 , 90)));
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 90)));
 
         front2.setLocation(OpenGLMatrix
                 .translation(-halfField, quadField, mmTargetHeight)
@@ -358,7 +364,7 @@ public class SkyStone_Auto extends LinearOpMode {
 
         rear1.setLocation(OpenGLMatrix
                 .translation(halfField, quadField, mmTargetHeight)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0 , -90)));
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, -90)));
 
         rear2.setLocation(OpenGLMatrix
                 .translation(halfField, -quadField, mmTargetHeight)
@@ -381,14 +387,14 @@ public class SkyStone_Auto extends LinearOpMode {
 
         // Rotate the phone vertical about the X axis if it's in portrait mode
         if (PHONE_IS_PORTRAIT) {
-            phoneXRotate = 90 ;
+            phoneXRotate = 90;
         }
 
         // Next, translate the camera lens to where it is on the robot.
         // In this example, it is centered (left to right), but forward of the middle of the robot, and above ground level.
-        final float CAMERA_FORWARD_DISPLACEMENT  = 4.0f * mmPerInch;   // eg: Camera is 4 Inches in front of robot center
+        final float CAMERA_FORWARD_DISPLACEMENT = 4.0f * mmPerInch;   // eg: Camera is 4 Inches in front of robot center
         final float CAMERA_VERTICAL_DISPLACEMENT = 8.0f * mmPerInch;   // eg: Camera is 8 Inches above ground
-        final float CAMERA_LEFT_DISPLACEMENT     = 0;     // eg: Camera is ON the robot's center line
+        final float CAMERA_LEFT_DISPLACEMENT = 0;     // eg: Camera is ON the robot's center line
 
         OpenGLMatrix robotFromCamera = OpenGLMatrix
                 .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
@@ -398,21 +404,128 @@ public class SkyStone_Auto extends LinearOpMode {
             ((VuforiaTrackableDefaultListener) trackable.getListener()).setPhoneInformation(robotFromCamera, CAMERA_CHOICE);
         }
 
-        return  targetsSkyStone;
+        return targetsSkyStone;
     }
 
 
+    private void pickup(){
+        // move the arm up
+        da.setTargetPosition(-2400);
+        da.setPower(0.6);
+        while(da.getCurrentPosition()>da.getTargetPosition() && opModeIsActive()){
+            telemetry.addData("da",da.getCurrentPosition());
+            telemetry.update();
+        }
+        da.setPower(0);
+
+        ta.setTargetPosition(700);
+        ta.setPower(0.4);
+        while(ta.getCurrentPosition()<ta.getTargetPosition() && opModeIsActive()){
+            telemetry.addData("ta",ta.getCurrentPosition());
+            telemetry.update();
+        }
+        ta.setPower(0);
+
+
+        md.setTargetPosition(-1500);
+        md.setPower(0.5);
+        while (md.getCurrentPosition()>md.getTargetPosition() && opModeIsActive()){
+            telemetry.addData("md",md.getCurrentPosition());
+            telemetry.update();
+        }
+        md.setPower(0);
+
+        //grab
+
+        da.setTargetPosition(0);
+        da.setPower(-0.25);
+        while(da.getCurrentPosition()<da.getTargetPosition() && opModeIsActive()){
+            telemetry.addData("da",da.getCurrentPosition());
+            telemetry.update();
+        }
+        da.setPower(0);
+
+        md.setTargetPosition(-1000);
+        md.setPower(-0.3);
+        while (md.getCurrentPosition()<md.getTargetPosition() && opModeIsActive()){
+            telemetry.addData("md",md.getCurrentPosition());
+            telemetry.update();
+        }
+        md.setPower(0);
+
+        ta.setTargetPosition(0);
+        ta.setPower(-0.25);
+        while(ta.getCurrentPosition()>ta.getTargetPosition() && opModeIsActive()){
+            telemetry.addData("ta",ta.getCurrentPosition());
+            telemetry.update();
+        }
+        ta.setPower(0);
+    }
+
+    private void drop(){
+
+        ta.setTargetPosition(750);
+        ta.setPower(0.4);
+        while(ta.getCurrentPosition()<ta.getTargetPosition() && opModeIsActive()){
+            telemetry.addData("ta",ta.getCurrentPosition());
+            telemetry.update();
+        }
+        ta.setPower(0);
+
+        md.setTargetPosition(-1800);
+        md.setPower(0.5);
+        while (md.getCurrentPosition()>md.getTargetPosition() && opModeIsActive()){
+            telemetry.addData("md",md.getCurrentPosition());
+            telemetry.update();
+        }
+        md.setPower(0);
+
+
+        strafeTo(-25,0.5);
+
+        moveTo(-10,0.5);
+
+
+
+        ta.setTargetPosition(0);
+        ta.setPower(-0.25);
+        while(ta.getCurrentPosition()>ta.getTargetPosition() && opModeIsActive()){
+            telemetry.addData("ta",ta.getCurrentPosition());
+            telemetry.update();
+        }
+        ta.setPower(0);
+
+        md.setTargetPosition(0);
+        md.setPower(-0.3);
+        while (md.getCurrentPosition()<md.getTargetPosition() && opModeIsActive()){
+            telemetry.addData("md",md.getCurrentPosition());
+            telemetry.update();
+        }
+        md.setPower(0);
+
+        da.setTargetPosition(0);
+        da.setPower(-0.25);
+        while(da.getCurrentPosition()<da.getTargetPosition() && opModeIsActive()){
+            telemetry.addData("da",da.getCurrentPosition());
+            telemetry.update();
+        }
+        da.setPower(0);
+
+
+
+    }
     /* OP MODE CODE */
+
 
     public void runOpMode() {
 
         //settings for the IMU
         BNO055IMU.Parameters param = new BNO055IMU.Parameters();
 
-        param.mode                = BNO055IMU.SensorMode.IMU;
-        param.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-        param.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        param.loggingEnabled      = false;
+        param.mode = BNO055IMU.SensorMode.IMU;
+        param.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        param.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        param.loggingEnabled = false;
 
         //map the motors and IMU
         fl = hardwareMap.get(DcMotor.class, "frontLeft");
@@ -423,6 +536,7 @@ public class SkyStone_Auto extends LinearOpMode {
         da = hardwareMap.get(DcMotor.class, "downArm");
         md = hardwareMap.get(DcMotor.class, "middleArm");
         imu = hardwareMap.get(BNO055IMU.class, "imu");
+        sv = hardwareMap.get(Servo.class, "servo");
 
         imu.initialize(param);
 
@@ -438,7 +552,7 @@ public class SkyStone_Auto extends LinearOpMode {
         fl.setDirection(DcMotorSimple.Direction.REVERSE);
         bl.setDirection(DcMotorSimple.Direction.REVERSE);
         br.setDirection(DcMotorSimple.Direction.REVERSE);
-
+        ta.setDirection(DcMotorSimple.Direction.REVERSE);
 
         //encoder settings
         ta.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -453,7 +567,7 @@ public class SkyStone_Auto extends LinearOpMode {
 
         VuforiaTrackables targetsSkyStone = detection();
 
-        //targetsSkyStone.activate();
+        targetsSkyStone.activate();
 
         //update status
         telemetry.addData("Status", "Initialized");
@@ -463,13 +577,133 @@ public class SkyStone_Auto extends LinearOpMode {
         waitForStart();
 
         //update status
-        telemetry.addData("Status", "Running");
+        telemetry.addData("Status", "Running2");
         telemetry.update();
 
-        resetAngle();
 
-        strafeTo(50, 0.5);
 
-        //targetsSkyStone.deactivate();
+
+        moveTo(60,1);
+
+
+
+        targetVisible = false;
+
+        double instrafed = 0;
+        VuforiaTrackable target;
+        while(!targetVisible && opModeIsActive()) {//!targetVisible && opModeIsActive()
+            for (VuforiaTrackable trackable : targetsSkyStone) {
+                if ((((VuforiaTrackableDefaultListener) trackable.getListener()).isVisible()) && trackable.getName() == "Stone Target") {
+                    telemetry.addData("Visible Target", trackable.getName());
+                    targetVisible = true;
+                    target = trackable;
+
+                    // getUpdatedRobotLocation() will return null if no new information is available since
+                    // the last time that call was made, or if the trackable is not currently visible.
+                    OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener) trackable.getListener()).getUpdatedRobotLocation();
+                    if (robotLocationTransform != null) {
+                        lastLocation = robotLocationTransform;
+                    }
+                    break;
+                }
+            }
+
+            if (targetVisible) {
+                // express position (translation) of robot in inches.
+                VectorF translation = lastLocation.getTranslation();
+                telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
+                        translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
+
+                // express the rotation of the robot in degrees.
+                Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
+                telemetry.addData("Rot (deg)", "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f", rotation.firstAngle, rotation.secondAngle, rotation.thirdAngle);
+                telemetry.update();
+            }
+            else {
+
+                if (instrafed > -70 && !strafed) {
+                    strafeTo(-10, 1);
+                    instrafed -= 10;
+                } else {
+                    strafed = true;
+                    strafeTo(20, 1);
+                    instrafed += 20;
+                }
+
+            }
+
+        }
+
+
+        telemetry.update();
+
+        VectorF translation = lastLocation.getTranslation();
+        telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
+                translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
+        strafeTo(((translation.get(1)/ mmPerInch)*2.54)+7,0.75);
+        telemetry.addData("strafed","done");
+        telemetry.update();
+        instrafed -= (-(translation.get(0) / mmPerInch)*2.54) - 32;
+
+        moveTo((-(translation.get(0) / mmPerInch)*2.54)-32,0.25);
+
+        pickup();
+
+
+        moveTo(-12,0.6);
+        turnTo(-90,0.4);
+        sv.setPosition(0.5);
+        telemetry.addData("strafed",instrafed);
+        telemetry.addData("moved",((-(translation.get(0) / mmPerInch)*2.54) - 32));
+        telemetry.update();
+
+        betterwait(4000);
+        moveTo(173+instrafed,0.8);
+
+        da.setTargetPosition(-1500);
+        da.setPower(0.6);
+        while(da.getCurrentPosition()>da.getTargetPosition() && opModeIsActive()){
+            telemetry.addData("da",da.getCurrentPosition());
+            telemetry.update();
+        }
+        da.setPower(0);
+
+        strafeTo(72 ,0.4);
+
+        drop();
+
+
+        turnTo(180,0.5);
+        strafeTo(-25,0.5);
+        moveTo(-10,0.5);
+        sv.setPosition(0);
+        betterwait(250);
+
+        moveTo(13,0.4);
+        turnTo(-45,0.4);
+        moveTo(-25,0.4);
+
+        /*
+        strafeTo(60,0.4);
+        turnTo(-25,0.4);
+        moveTo(12,0.4);
+        strafeTo(60,0.4);
+        turnTo(-25,0.4);
+        moveTo(6,0.4);
+        strafeTo(30,0.4);
+        turnTo(-25,0.4);*/
+
+
+        sv.setPosition(0.5);
+        betterwait(500);
+
+        strafeTo(30,1);
+        moveTo(35,1);
+
+        targetsSkyStone.deactivate();
+
+
+
     }
+
 }
